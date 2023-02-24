19d8e4fb1407f38bad3a562cbae4863a1f9f0885
koruri29 <koruri821@yahoo.co.jp>
2023-02-24 11:21:56 +0900

    微修正

--- htdocs/try56.php
+++ htdocs/try56.php
@@ -5,14 +5,13 @@ require_once 'model.php';
 
 $product_data = [];
 
-define('DSN', 'mysql:host=mysql34.conoha.ne.jp;dbname=bcdhm_omiya_pf0006;');
-define('LOGIN_USER', 'bcdhm_omiya_pf0006');
-define('PASSWORD', 'N3p!CxYc');
-$pdo = new PDO(DSN, LOGIN_USER, PASSWORD);
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// define('DSN', 'mysql:host=mysql34.conoha.ne.jp;dbname=bcdhm_omiya_pf0006;');
+// define('LOGIN_USER', 'bcdhm_omiya_pf0006');
+// define('PASSWORD', 'N3p!CxYc');
+// $pdo = new PDO(DSN, LOGIN_USER, PASSWORD);
+// $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-// $pdo = get_connection();
-var_dump($pdo);
+$pdo = get_connection();
 $product_data = get_product_list($pdo);
 $product_data = h_array($product_data);
 
